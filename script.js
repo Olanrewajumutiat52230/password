9acb8ffa281d83647641804ceef2d246f07c3088
mutyatolanrewaju@gmail.com <mutyatolanrewaju@gmail.com>
2025-04-15 07:35:46 +0100

    passwordu

---
+++ script.js
@@ -0,0 +1,17 @@
+function check() {
+    let password = document.getElementById('password').value
+    let confirm_password = document.getElementById('confirm-password').value
+
+if( password === '') {
+document.getElementById('demo1').innerText = 'fill in the blank spaces'
+}
+
+else if (confirm_password === password ) {
+  alert('Access granted') 
+} else {
+    alert('Access denied');
+}
+
+}  
+
+
